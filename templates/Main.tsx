6b62430d61c38c6ce9d2e36a682c6eefa10e25b8
Minh Khuy <haokhuy@gmail.com>
2022-09-25 16:30:34 +0700

    Config

---
+++ templates/Main.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import type { ReactNode } from "react";
+
+import { AppConfig } from "../utils/AppConfig";
+
+type IMainProps = {
+  meta: ReactNode;
+  children: ReactNode;
+};
+
+const Main = (props: IMainProps) => (
+  <div className="w-full px-1 text-gray-700 antialiased">
+    {props.meta}
+    {props.children}
+  </div>
+);
+
+export { Main };
